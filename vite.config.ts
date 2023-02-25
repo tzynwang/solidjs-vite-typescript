49a2c4072947445fba77a77b00f2fed628cab882
Tzu Yin <tzyn.wang@gmail.com>
2023-02-25 18:44:51 +0800

    [feat] vite.config use env.VITE_BASE

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [solidPlugin()],
+    base: env.VITE_BASE,
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+    },
+  };
 });
