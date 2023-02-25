7d5af6119a0d07893d2cc984dd79c89105f14ca2
Tzu Yin <tzyn.wang@gmail.com>
2023-02-25 19:40:15 +0800

    [feat] Common/Navigate

---
+++ src/component/Common/Navigate/index.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'solid-js';
+import { A } from '@solidjs/router';
+import { ROUTE } from '../../../model/GeneralModel';
+import cssModule from './index.module.css';
+
+const Navigate: Component = () => {
+  /* Main */
+  return (
+    <nav class={cssModule.wrapper}>
+      <A href={ROUTE.home}>home</A>
+      <A href={ROUTE.blog} end>
+        blog
+      </A>
+    </nav>
+  );
+};
+
+export default Navigate;
